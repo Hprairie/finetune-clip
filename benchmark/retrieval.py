fixes to benchmark masking

benchmark/retrieval.py
@@ -96,7 +96,6 @@ def finegrained_recall_at_k(
     
     print(image_encodings.shape)
     print(text_encodings.shape)
-    print(masks.shape)
 
     # Create a hnswlib index for the image embeddings
     dim = image_encodings.shape[1]
@@ -118,13 +117,14 @@ def finegrained_recall_at_k(
             
             # Get the text encodings for this caption
             caption_text_encodings = text_encodings[caption_idx * 77 : (caption_idx + 1) * 77]
+            
 
             # Images that we want to run full maxsim on
             image_matches = set()
             
             # For each token in the caption excluding padding tokens
             for token_idx in range(caption_idx * 77, (caption_idx + 1) * 77):
-                if len(masks) > 0 and masks[caption_idx][token_idx - (caption_idx * 77)] == 0:
+                if masks is not None and masks[caption_idx][token_idx - (caption_idx * 77)] == 0:
                     break
 
                 # Query the kNN for n_patches closest patches
@@ -151,7 +151,11 @@ def finegrained_recall_at_k(
                 cos_sim = scaled_text_embeddings @ scaled_image_embeddings.T
                 
                 # Max pool per embedding
-                score = torch.max(cos_sim, dim=1).values.sum()
+                if masks is not None:
+                    score = torch.max(cos_sim, dim=1).values.mean()
+                else:
+                    score = torch.max(cos_sim, dim=1).values.sum()
+
                 top_k_images[match] = score
 
             # Get the top k images by score
@@ -213,6 +217,9 @@ def reranker_recall_at_k(
             # For all found images calculate the similarity scores
             top_k_images = collections.defaultdict(float)
             caption_text_encodings = fg_text_encodings[caption_idx * 77 : (caption_idx + 1) * 77]
+            if masks is not None:
+                caption_mask = masks[caption_idx * 77 : (caption_idx + 1) * 77]
+                caption_text_encodings = caption_text_encodings * caption_mask.unsqueeze(-1)
 
             for match in image_matches:
                 match = int(match)