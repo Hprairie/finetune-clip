981f2f6bdb359eb716950ed75559936236f65499
Hprairie <hprairie17@gmail.com>
2024-05-13 11:46:16 -0500

    more flexible convert_dataset.py

--- convert_dataset.py
+++ convert_dataset.py
@@ -5,9 +5,11 @@ from torchvision.datasets import CocoCaptions
 
 # This is just for me 
 dataset_path = os.environ['DATASETS']
+annotations = "captions_val2017"
+image_folder = "val2017"
 
 image_path = f"{dataset_path}/coco/images" # set this to smth meaningful
-ann_path = f"{dataset_path}/coco/annotations/captions_train2017.json"
+ann_path = f"{dataset_path}/coco/annotations/{annotations}.json"
 ds = CocoCaptions(
         root=image_path, 
         annFile=ann_path,
@@ -20,9 +22,9 @@ for img in imgs:
     caps = coco.imgToAnns[img["id"]]
     for cap in caps:
         future_df["filepath"].append(
-            os.path.join(f'{dataset_path}/coco/images/train2017', img["file_name"])
+            os.path.join(f'{dataset_path}/coco/images/{image_folder}', img["file_name"])
         )
         future_df["title"].append(cap["caption"])
 pd.DataFrame.from_dict(future_df).to_csv(
-  os.path.join(f"{dataset_path}/coco/annotations", "captions_train2017.csv"), index=False, sep="\t"
+  os.path.join(f"{dataset_path}/coco/annotations", f"{annotations}.csv"), index=False, sep="\t"
 )
